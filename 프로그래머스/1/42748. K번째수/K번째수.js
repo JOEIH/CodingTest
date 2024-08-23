05ca4eface557afddfe40798b982833bc51b9fb8
Hyun <108066931+JOEIH@users.noreply.github.com>
2024-08-24 03:32:37 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,12 @@
+function solution(array, commands) {
+    let answer = []
+    for(let i = 0; i < commands.length; i++) {
+        let num = []
+        let arr = commands[i];
+        num = array.slice(arr[0] - 1, arr[1]);
+        num.sort((a,b) => a - b);
+        answer.push(num[arr[2]-1])
+        
+    }
+    return answer
+}
