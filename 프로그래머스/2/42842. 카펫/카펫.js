cfe46a41d6097e8909136a8782330007835aeff6
Hyun <108066931+JOEIH@users.noreply.github.com>
2024-09-03 00:44:23 +0900

    [level 2] Title: 카펫, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -3,19 +3,14 @@ function solution(brown, yellow) {
     let total = brown + yellow;
     let w = 0, h = 0;
     
-    //if(Math.sqrt(total)**2 == total) {
-       // w = h = Math.sqrt(total)
-        //return answer = [w, h]
-    //} else {
-        for (let i = 1; i < total; i++) {
-            if(total % i == 0) {
-                h = i;
-                w = total / i;
-            }
-            
-            if(yellow == (w-2)*(h-2) && w >= h) {
-               return answer = [w, h]
-            }
+    for (let i = 1; i < Math.floor(total); i++) {
+        if(total % i == 0) {
+            h = i;
+            w = total / i;
         }
-   // } 
+            
+        if(yellow == (w-2)*(h-2) && w >= h) {
+            return answer = [w, h]
+        }
+    }
 }
