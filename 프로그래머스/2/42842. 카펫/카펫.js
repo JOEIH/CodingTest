da7c8804ed241bde3a93f9fda15804ee7709af87
Hyun <108066931+JOEIH@users.noreply.github.com>
2024-09-04 19:00:49 +0900

    [level 2] Title: 카펫, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -3,10 +3,9 @@ function solution(brown, yellow) {
     let total = brown + yellow;
     let w = 0, h = 0;
     
-    for (let i = 1; i < Math.floor(total); i++) {
-        if(total % i == 0) {
-            h = i;
-            w = total / i;
+    for (let h = 3; h < total; h++) {
+        if(total % h == 0) {
+            w = total / h;
         }
             
         if(yellow == (w-2)*(h-2) && w >= h) {
