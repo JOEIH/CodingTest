3dbbb85655de2f6c6958b090cb076f98729193cd
Hyun <108066931+JOEIH@users.noreply.github.com>
2024-10-08 03:29:27 +0900

    [level 2] Title: [3차] 압축, Time: 2.23 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -0,0 +1,27 @@
+function solution(msg) {
+    var answer = []; //출력할 배열
+    let dict = [0, "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; //사전 초기화 인덱스 1번부터 시작
+    
+    let i = 0;
+    let word = msg[i]; 
+    while(i !== msg.length) { //마지막 인덱스에 도달할 때까지
+	    idx = dict.indexOf(word) 
+	    
+	    if (idx !== -1) {
+            if (i + 1 == msg.length) {
+                answer.push(idx)
+                return answer
+            } else {
+                word += msg[i+1];
+                i++;
+            }
+	    } else {
+		    dict.push(word)
+		    let preWord = word.slice(0, -1);
+		    pre = dict.indexOf(preWord)
+		    answer.push(pre);
+		    word = msg[i];
+	    }
+    }
+    return answer;
+}
