356739f786af7e60b2cf2033ed9f27070167abbb
Hyun <108066931+JOEIH@users.noreply.github.com>
2024-10-10 05:05:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.97 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,23 +1,23 @@
 function solution(participant, completion) {
-    let hash = {}
-    
-    participant.forEach((el) => {
-        if(!hash[el]) {
-            hash[el] = 1
-        } else {
-            hash[el]++
-        }
-    })
-    
-    completion.forEach((el) => {
-        if(hash[el]) {
-            hash[el]--;
-        }
-    })
-    
-    for(let i in hash) {
-        if(hash[i] === 1) {
-            return i
-        }
+  const participantSet = [...new Set(participant)];
+  const completionSet = [...new Set(completion)];
+
+  if (participantSet.length !== completionSet.length) { 
+    //동명이인이 아닌 사람이 미완주자일 때
+    for (let i = 0; i < participantSet.length; i++) {
+      if (!completionSet.includes(participantSet[i])) {
+        return participantSet[i]
+      } 
     }
+  } else {  //동명이인인 사람이 미완주자일 때 
+    const idxArr = [];
+    for (let i = 0; i < participant.length; i++) {
+        participant.map((el, idx) => {
+            if (el === completion[i] && !idxArr.includes(idx)) {
+                idxArr.push(idx);
+            }
+        })
+    }
+    return participant[idxArr[idxArr.length-1]];
+  }
 }
