4a27a69d7ff395682b9574c1a465119c01aa9156
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-02-25 16:34:29 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,15 +1,4 @@
 function solution(emergency) {
     const sorting = [...emergency].sort((a, b) => b - a)
-    let sortingMap = new Map()
-    let answer = []
-    
-    sorting.forEach((v, index) => {
-        sortingMap.set(v, index + 1)
-    })
-    
-    answer = emergency.map((v) => {
-        return v = sortingMap.get(v)
-    })
-
-    return answer;
+    return emergency.map((v) => sorting.indexOf(v) + 1);
 }
