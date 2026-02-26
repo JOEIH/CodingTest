f43db265e2f82dd55df17eb6b665279fab20518e
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-02-26 18:17:21 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,9 +1,9 @@
 function solution(rsp) {
     const win = {
-        '2': 0,
-        '0': 5,
-        '5': 2
+        2: 0,
+        0: 5,
+        5: 2
     }
     
-    return rsp.split('').map((v) => win[v]).join('');
+    return [...rsp].map((v) => win[v]).join('');
 }
