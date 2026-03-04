dfd3523473a8d3709086180c2d7cf9957dc45229
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-04 23:19:54 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -3,7 +3,7 @@ function solution(my_string) {
     
     for (let ch of my_string) {
         let numCh = Number(ch)
-        if (Number.isInteger(numCh)) answer.push(numCh)
+        if (!isNaN(numCh)) answer.push(numCh)
     }
     
     return answer.sort((a, b) => a - b);
