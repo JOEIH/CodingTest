ae435520e394a301c1176040deabc3bc116d9ed0
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-06 01:51:23 +0900

    [level 0] Title: 369게임, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,15 +1,15 @@
 function solution(order) {
     var answer = 0
-    const arr = order.toString().split('').map(v => Number(v))
+    const arr = order.toString().split('')
     
     for (let i of arr) {
-        if (i === 9) {
+        if (i === '9') {
             answer += 1
             continue
-        } else if (i === 6) {
+        } else if (i === '6') {
             answer += 1
             continue
-        } else if (i === 3) {
+        } else if (i === '3') {
             answer += 1
             continue
         }
