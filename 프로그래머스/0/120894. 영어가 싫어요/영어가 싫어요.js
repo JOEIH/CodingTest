453296922f1b550ed79ce2c74a8d2fc8c0851671
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-09 21:46:03 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,30 +1,21 @@
 function solution(numbers) {
-    const map = new Map([
-        ['zero', 0],
-        ['one', 1],
-        ['two', 2],
-        ['three', 3],
-        ['four', 4],
-        ['five', 5],
-        ['six', 6],
-        ['seven', 7],
-        ['eight', 8],
-        ['nine', 9],
-    ])
+    const map = new Map(
+        ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].map((k, i) => [k, i.toString()])
+    );
     
-    let answer = []
-    // let answer = ''
+    // let answer = []
+    let answer = ''
     let cur = ''
     
     for (let ch of numbers) {
         cur += ch
         if (map.has(cur)) {
-            answer.push(map.get(cur))
-            // answer += map.get(cur)
+            // answer.push(map.get(cur))
+            answer += map.get(cur)
             cur = ''
         }
     }
 
-    return parseInt(answer.join(''), 10);
-    // return parseInt(answer, 10)
+    // return parseInt(answer.join(''), 10);
+    return parseInt(answer, 10)
 }
