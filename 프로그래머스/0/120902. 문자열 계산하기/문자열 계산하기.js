c38636ab08d26e2e31d7cd504bfdfcee433dade0
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-10 03:47:22 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,24 +1,15 @@
 function solution(my_string) {
-    const arr = my_string.split(' ')
-    let answer = arr[0]
+    // 공백 기준으로 나누기 때문에 항상 [숫자, 연산자, 숫자, 연산자...] 형태
+    const arr = my_string.split(' ') 
+    let answer = Number(arr[0])
     
-    for (let i = 1; i < arr.length; i++) {
-        if (Number.isInteger(arr[i])) {
-            answer += arr[i]
-            continue
-        } 
-        
-        if (arr[i - 1] === '+') {
-            answer = Number(answer) + Number(arr[i])
-            continue
-        }
-        
-        if (arr[i - 1] === '-') {
-            answer = Number(answer) - Number(arr[i])
-            continue
+    for (let i = 1; i < arr.length; i+=2) {
+        if (arr[i] === '+') {
+            answer += Number(arr[i + 1])
+        } else if (arr[i] === '-') {
+            answer -= Number(arr[i + 1])
         }
     }
    
-    
     return answer;
 }
