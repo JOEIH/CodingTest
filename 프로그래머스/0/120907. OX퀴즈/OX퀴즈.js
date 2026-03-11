2115a5699f2d9d0199539b3bccda67b54bbab8b9
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-12 02:16:40 +0900

    [level 0] Title: OX퀴즈, Time: 0.08 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -1,14 +1,16 @@
 function solution(quiz) {
     var answer = [];
     
-    for (let i of quiz) {
-        const arr = i.split(' ');
-        const [num1, operator, num2, equal, result] = arr;
-        
-        const calculate = operator === '+' ? Number(num1) + Number(num2) : Number(num1) - Number(num2);
-        
-        answer.push(calculate === Number(result) ? 'O' : 'X');
-    }
+     for (let i of quiz) {
+         const arr = i.split(' ');
+         
+         const front = Number(arr[0]);
+         const back = Number(arr[2]);
+         
+         const result = arr[1] === '+' ? front + back : front - back;
+         
+         result === Number(arr[4]) ? answer.push('O') : answer.push('X');
+     }
     
     return answer;
 }
