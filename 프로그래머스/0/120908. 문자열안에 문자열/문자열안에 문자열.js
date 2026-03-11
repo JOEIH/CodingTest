a3bd8b7c941342becc9442a32e9bc08a23825ba3
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-12 03:11:36 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,17 +1,3 @@
 function solution(str1, str2) {
-    let answer = 2;
-    let cur = '';
-    
-    for (let i of str1) {
-        cur += i
-        
-        if (cur.indexOf(str2) === -1) {
-            continue;
-        } else {
-            answer = 1;
-            break;
-        }
-    }
-    
-    return answer;
+    return str1.split(str2).length >= 2 ? 1 : 2;
 }
