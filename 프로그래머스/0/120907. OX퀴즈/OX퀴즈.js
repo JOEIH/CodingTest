e4a10e4519113c70eae7ea86feed93dd774cf6b7
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-12 01:43:47 +0900

    [level 0] Title: OX퀴즈, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,16 @@
+function solution(quiz) {
+    var answer = [];
+    
+     for (let i of quiz) {
+         const arr = i.split(' ');
+         
+         const front = Number(arr[0]);
+         const back = Number(arr[2]);
+         
+         const result = arr[1] === '+' ? front + back : front - back;
+         
+         i = result === Number(arr[4]) ? answer.push('O') : answer.push('X');
+     }
+    
+    return answer;
+}
