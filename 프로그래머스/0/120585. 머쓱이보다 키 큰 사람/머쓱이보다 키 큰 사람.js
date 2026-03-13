7f7315abffe18b620d837b36269c1a9a76c5f150
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-13 16:52:10 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,3 +1,12 @@
 function solution(array, height) {
-    return array.filter(v => v > height).length
+    array.sort((a, b) => a - b);
+    
+    let curIndex = 0
+    // 키가 같은 사람도 고려
+    // 키가 같으면 반복문 탈출
+    while (height >= array[curIndex]) {
+        curIndex++;
+    }
+        
+    return array.length - curIndex;
 }
