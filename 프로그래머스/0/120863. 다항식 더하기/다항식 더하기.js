8a65f45617fda9ee03e657303ac9ea36328a228e
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-15 17:17:25 +0900

    [level 0] Title: 다항식 더하기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -2,18 +2,19 @@ function solution(polynomial) {
     let num = 0;
     let xCount = 0;
     
-    for (let i of polynomial.split(' ')) {
-        if (i[i.length - 1] === 'x') {
-            let frontNum = i.slice(0, i.length - 1)
-            frontNum === '' ? xCount += 1 : xCount += Number(frontNum);
-        } else if (i !== '+') {
+    for (let i of polynomial.split(' + ')) {
+        if (i.includes('x')) {
+            let frontNum = i.replace('x', '')
+            xCount += frontNum === '' ? 1 : Number(frontNum);
+        } else {
             num += Number(i)
         }
     }
     
-    xCount = xCount === 1 ? '' : xCount;
+    let answer = [];
     
-    if (num !== 0 && xCount !== 0) return `${xCount}x + ${num}`
-    if (num === 0) return `${xCount}x`
-    if (xCount === 0) return `${num}`
+    if (xCount !== 0) answer.push(xCount === 1 ? 'x' : `${xCount}x`)
+    if (num !== 0) answer.push(num)
+    
+    return answer.join(' + ')
 }
