5ad628e6a44147759a32d264d3689a3750fcd922
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-15 16:21:00 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,8 +1,4 @@
 function solution(numbers) {
     numbers.sort((a, b) => a - b);
-    
-    let front = numbers[0] * numbers[1];
-    let back = numbers[numbers.length - 1] * numbers[numbers.length - 2];
-    
-    return front > back ? front : back;
+    return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 1] * numbers[numbers.length - 2]);
 }
