0ddab1b2da0a6ad9ee21aa73b5d23651d7f4d1cc
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-25 01:04:55 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,16 +1,4 @@
 function solution(A, B) {
     if (A === B) return 0;
-    
-    let a = A.split('');
-    let count = 0;
-    
-    while (count !== a.length - 1) {
-        if (a.join('') === B) break;
-        
-        let popped = a.pop();
-        a.unshift(popped);
-        count++;
-    }
-    
-    return a.join('') === B ? count : -1;
+    return (B + B).indexOf(A);
 }
