1ad336439e6e3ddce76a4862acccda9b5f2cd617
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-27 01:51:10 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,21 +1,15 @@
 function solution(a, b) {
-    var answer = 0;
-    
     const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
     const calculated = gcd(a, b);
     
     let lower = b / calculated;
-    
-    if (lower % 2 === 0) {
-        while (lower % 2 === 0) {
-            lower /= 2;
-        }
+
+    while (lower % 2 === 0) {
+        lower /= 2;
     }
-    
-    if (lower % 5 === 0) {
-        while (lower % 5 === 0) {
-            lower /= 5;
-        }
+
+    while (lower % 5 === 0) {
+        lower /= 5;
     }
     
     return lower === 1 ? 1 : 2;
