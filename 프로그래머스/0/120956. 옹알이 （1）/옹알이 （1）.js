bd4860d933ee704a480b54d943349e13b5cc00b4
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-27 12:48:01 +0900

    [level 0] Title: 옹알이 (1), Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -2,7 +2,7 @@ function solution(babbling) {
     const babble = ['aya', 'ye', 'woo', 'ma'];
     
     for (let i of babble) {
-        babbling = babbling.map(v => v.includes(i) ? v = v.replace(i, ' ') : v)
+        babbling = babbling.map(v => v.replace(i, ' '))
     }
     
     return babbling.filter(v => v.trim().length === 0).length;
