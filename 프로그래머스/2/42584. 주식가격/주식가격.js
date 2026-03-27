f61d330040fdcf68a6610282bff2fa91f2168427
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-03-27 13:46:00 +0900

    [level 2] Title: 주식가격, Time: 28.55 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,15 +1,21 @@
 function solution(prices) {
-    const answer = new Array(prices.length).fill(0);
+  const answer = new Array(prices.length).fill(0);
+  const stack = []; 
 
-    for (let i = 0; i < prices.length; i++) {
-        for (let j = i + 1; j < prices.length; j++) {
-            answer[i]++; 
-            
-            if (prices[i] > prices[j]) {
-                break;
-            }
-        }
+  for (let i = 0; i < prices.length; i++) {
+    while (stack.length > 0 && prices[stack[stack.length - 1]] > prices[i]) {
+      const past_time = stack.pop(); 
+      answer[past_time] = i - past_time; 
     }
+    
+    // 2. 폭락한 애들 다 쫓아냈으니, 지금 시간(i)도 대기열에 추가
+    stack.push(i);
+  }
 
-    return answer;
+  while (stack.length > 0) {
+    const past_time = stack.pop();
+    answer[past_time] = (prices.length - 1) - past_time;
+  }
+
+  return answer;
 }
