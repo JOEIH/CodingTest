d0ac953e11edb04a002487577c88bbe1f867a8e0
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-04-17 01:46:48 +0900

    [level 2] Title: 전화번호 목록, Time: 471.83 ms, Memory: 97.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,15 @@
+function solution(phone_book) {
+    let map = new Map()
+    
+    phone_book.forEach(v => map.set(v, true))
+    
+    for (let i of phone_book) {
+        let check = ''
+        for (let ch of i) {
+            check += ch;
+            if (check !== i && map.has(check)) return false;
+        }
+    }
+    
+    return true;
+}
