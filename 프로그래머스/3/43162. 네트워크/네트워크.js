362639477a2e70a9b09bd4cf29cf2c981304d14a
Hyun <108066931+JOEIH@users.noreply.github.com>
2026-04-24 17:17:16 +0900

    [level 3] Title: 네트워크, Time: 0.80 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,23 @@
+function solution(n, computers) {
+    let answer = 0;
+    let visited = new Set();
+    
+    function dfs(node) {
+        visited.add(node);
+        for (let i = 0; i < n; i++) {
+            if (computers[node][i] && !visited.has(i)) {
+                visited.add(i);
+                dfs(i);
+            }
+        }
+    }
+    
+    for (let i = 0; i < n; i++) {
+        if (!visited.has(i)) {
+            dfs(i);
+            answer++;
+        }
+    }
+    
+    return answer;
+}
